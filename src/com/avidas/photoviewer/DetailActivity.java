a85b88a6d6942649c8326da2b22098bd975fce6e
Avi Das <adas@colgate.edu>
2014-05-10 22:10:16 -0500

    Add Detail View Activity

--- src/com/avidas/photoviewer/DetailActivity.java
+++ src/com/avidas/photoviewer/DetailActivity.java
@@ -1,5 +1,23 @@
 package com.avidas.photoviewer;
 
-public class DetailActivity {
-
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+ 
+public class DetailActivity extends Activity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_detail);
+        // get intent data
+        Intent i = getIntent();
+        // Selected image id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+ 
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
 }
